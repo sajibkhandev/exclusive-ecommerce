9e07fd027c601d8d2bc0a870e4b7cd427bd77547
sajibkhandev <sajibkhan.mern@gmail.com>
2026-02-20 19:14:36 +0600

    feat: add navber part

--- src/components/ListItem.jsx
+++ src/components/ListItem.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const ListItem = ({text,className}) => {
   return (
-    <li className={`relative inline-block text-[#000000] text-base font-pop font-normal cursor-pointer after:w-[0px] after:h-[1px] after:bg-[#000000] after:absolute after:left-0 after:bottom-0 after:content-[""]  hover:after:w-full after:duration-300 ${className}`}>{text}</li>
+    <li className={`relative inline-block text-[#000000] text-base font-pop font-normal cursor-pointer after:w-0 after:h-px after:bg-[#00000080] after:absolute after:left-0 after:bottom-0 after:content-[""]  hover:after:w-full after:duration-300 ${className}`}>{text}</li>
   )
 }
 
--- src/layouts/Navber.jsx
+++ src/layouts/Navber.jsx
@@ -4,16 +4,19 @@ import Flex from '../components/Flex'
 import Image from '../components/Image'
 import Logo from '../assets/logo.png'
 import ListItem from '../components/ListItem'
+import { FiSearch } from "react-icons/fi";
+import { GoHeart } from 'react-icons/go'
+import { IoCartOutline } from 'react-icons/io5'
 
 const Navber = () => {
     return (
-        <nav>
+        <nav className='pt-10 pb-4 border-b border-[#0000004d]'>
             <Container>
-                <Flex>
+                <Flex className='items-center'>
                     <div className='w-3/12 '>
                     <Image src={Logo} alt="Logo" />
                     </div>
-                    <div className='w-6/12 '>
+                    <div className='w-5/12 '>
                     <ul className='flex gap-x-12'>
                         <ListItem  text="Home"/>
                         <ListItem  text="Contact"/>
@@ -23,7 +26,15 @@ const Navber = () => {
                        
                     </ul>
                     </div>
-                    <div className='w-3/12 '>button</div>
+                    <div className='w-4/12 flex gap-x-10 items-center'>
+                       <div className='relative w-[243px] bg-[#F5F5F5] rounded'>
+                        <input className='w-full py-2.5 pl-5 pr-10  text-sm placeholder:text-[#00000080] placeholder:text-xs placeholder:font-normal placeholder:font-pop' type="text" placeholder='What are you looking for?'/>
+                        <FiSearch className='absolute top-1/2 -translate-y-1/2 right-3 text-xl'/>
+                       </div>
+                      <GoHeart className='text-2xl'/>
+                       <IoCartOutline className='text-[26px]'/>
+
+                    </div>
                 </Flex>
             </Container>
         </nav>
