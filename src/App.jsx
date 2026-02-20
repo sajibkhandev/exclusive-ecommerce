988c8b3d5e4742485a8580f8e9b2abc35f6706ef
sajibkhandev <sajibkhan.mern@gmail.com>
2026-02-20 19:56:34 +0600

    feat: add footer part

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
 import React from 'react'
 import Shop from './layouts/Shop'
 import Navber from './layouts/Navber'
+import Footer from './layouts/Footer'
 
 const App = () => {
   return (
     <div>
       <Shop/>
       <Navber/>
+      <Footer/>
       
     </div>
   )
