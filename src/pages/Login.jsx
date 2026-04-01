210f298e5f16c9cb5deff421da97dd3924ac2e57
unknown <kaberichakrabarttykoushiki@gmail.com>
2026-04-01 21:31:56 +0600

    login completed

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -2,7 +2,14 @@ import React from 'react'
 
 const Login = () => {
   return (
-    <div>Login</div>
+    <>
+
+     <h1 className='bg-red-500'>Login</h1>
+    <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Hic totam repellendus asperiores debitis, fugit ipsam amet recusandae laudantium natus, eum, ab reprehenderit quibusdam eligendi et delectus doloribus excepturi praesentium. Quam ipsa soluta dignissimos reiciendis nihil quos voluptatum est deserunt beatae.</p>
+
+    </>
+   
+   
   )
 }
 
